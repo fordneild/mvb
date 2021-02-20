6525eeeb57408a0b13b6d844da316151f15821cc
Ford neild <fordneild@Fords-MacBook-Pro.local>
2021-02-20 18:28:29 -0500

    day 2

--- driver.py
+++ driver.py
@@ -1,14 +1,20 @@
 import threading
+import hashlib
+# from pynacl.signing import SigningKey
 import time
 import queue
 import json
 import sys
 import random
+import copy
 
 # these varibales are globally accessible to all nodes
 stop_threads = False
 nodes = {}
+# when we detect we are on a fork, add back transtactions from shorter block that are also not on the larger block
 unverifiedTxs = {}
+# once the longest chain has all of the transactions, we are done
+txInLongestChain = set()
 
 # driver takes transactions and number of nodes to process them
 
@@ -31,59 +37,184 @@ def driver(txs, numNodes):
 
 # Each node wraps a thread
 
+def generate_hash(secrets):
+    dk = hashlib.sha256()
+    for s in secrets:
+        bsecret = s.encode('utf-8')
+        dk.update(bsecret)
+    return dk.hexdigest
+
+class Transaction():
+    def __init__(self, tx):
+        # TODO validate tx
+        print("creating tx from " + str(tx))
+        Transaction.validate(tx)
+        self.input = tx['input']
+        self.number = tx['number']
+        self.output = tx['output']
+        self.sig = tx['sig']
+
+    def netTx(tx):
+        bank = {}
+        for i in tx.input:
+            output = i['output']
+            senderPk = output['pubkey']
+            sendAmount = output['value']
+            if(senderPk not in bank):
+                bank[senderPk] = 0
+            bank[senderPk] -= sendAmount
+        for o in tx.output:
+            recieverPk = o['pubkey']
+            recieveAmount = o['value']
+            if(recieverPk not in bank):
+                bank[recieverPk] = 0
+            bank[recieverPk] += recieveAmount
+        return bank
+
+    def validate(tx):
+        #
+        # if not valid, throw error
+        hexHash = generate_hash([tx['input'], tx['output'], tx['sig']])
+        if (tx['number'] != hexHash):
+            return False
+        totalInOut = 0
+        for val in Transaction.netTx(tx):
+            totalInOut+=val
+        if(totalInOut != 0):
+            return False
+
+class Block():
+    def __init__(self, tx: Transaction, prev):
+        self.tx = tx
+        self.prev = prev
+        self.nonce = self.generateNonce()
+        self.pow = self.generatePow()
+
+    def generateNonce(self):
+        # A nonce is an abbreviation for "number only used once," which is a number added to a hashed—or encrypted—block in a blockchain that, when rehashed, meets the difficulty level restrictions
+        return 'nonce'
+
+    def generatePow(self):
+        #  "pow": <the proof-of-work, a hash of the tx, prev, and nonce fields>
+        return 'pow'
+
+    def hash(self):
+        return generate_hash(self)
+        # return generate_hash([self.tx, self.prev, self.nonce, self.pow])
+
+
+class Chain():
+        def __init__(self, genesisBlock: Block):
+            self.blocks = [genesisBlock]
+            # a set of tx in the chain
+            self.tx = set()
+            # the current state of everyone wallet
+            # <pk>:<totalCoins>
+            self.bank = {}
+
+        def addTx(self, rawTx):
+            tx = Transaction(rawTx)
+            net = Transaction.netTx(rawTx)
+            for(userPk, delta) in net.items():
+                if(userPk not in self.bank):
+                    self.bank[userPk] = 0
+                if(delta<0):
+                    if(self.bank[userPk] < delta*-1):
+                        raise Exception("user " + userPk + " does not have enough money")
+            
+
+            # for each person that lost money from this transaction
+            # if they dont have the money they lost
+            # dont add the transaction to our chain
+
+            lastBlockHash = self.blocks[len(self.blocks) - 1].hash()
+            block = Block(tx, lastBlockHash)
+            # create tx
+            # create block
+            # validate block (they have the money for it in bank)
+            # add the block to our chain
+            # add tx to self.tx
+            # update bank
+            return 'nonce'
+
+        def hasTx(self, txNum):
+            return txNum in self.tx
+
+        def validate(blocks, genesisBlock):
+            try:
+                if(hash(blocks[0]) != hash(genesisBlock)):
+                    return False
+                # we have the same genesis
+                for block in blocks[1:]:
+                    if(block.pow < 0x07FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF):
+                        pass
+            except:
+                return False
 
 class Node(threading.Thread):
-    def __init__(self, name):
+    def __init__(self, name, genesisBlock):
         threading.Thread.__init__(self)
-        self.name = name
-        # Each Node has a Queue to track incoming blocks from other
-        self.q = queue.Queue()
-        self.seenTxNums = set()
+        self.name=name
+        # Each Node has a Queue to track incoming chains from others
+        self.q=queue.Queue()
+        # we use these two to determine which unverified transaction we should look at next...
+        self.invalidTx=set()
+        # each node holds thier own chain
+        self.chain=Chain(genesisBlock)
 
     def run(self):
         # target function of the thread class
         try:
             print(self.name + " running...")
             while True:
-                # process all new blocks in queue
+                # process all new chains in queue
                 while(not self.q.empty()):
-                    self.processBlock()
+                    self.recieveChain()
                 # process any new unverified transactions
-                self.processUnverifiedTransactions()
+                self.processUnverifiedTx()
                 global stop_threads
                 if stop_threads:
                     break
         finally:
             print(self.name + " stopped")
 
-    def processUnverifiedTransactions(self):
+    # gets a new unverified transaction not already in out chain
+    def processUnverifiedTx(self):
         global unverifiedTxs
+        # loop over all unverified Tx
         for (unverifiedTxNum, unverifiedTx) in unverifiedTxs.items():
-            if(unverifiedTxNum not in self.seenTxNums):
-                self.seenTxNums.add(unverifiedTxNum)
-                self.processUnverifiedTransaction(unverifiedTx)
-                # important that we only process one new transaction
-                # other nodes mat have new blocks for us
-                break
+            # until we find possible valid Tx that we do not alread have in our chain
+            if(not self.chain.hasTx(unverifiedTxNum) and unverifiedTxNum not in self.invalidTx):
+                try:
+                    tx=Transaction(unverifiedTx)
+                except:
+                    # this was an invalidTX, mark it as such
+                    self.invalidTx.add(unverifiedTxNum)
+                    # lets see if we got new chains from our neighbors
+                    break
+                try:
+                    self.chain.addTx(tx)
+                    self.broadcastChain()
+                except:
+                    # this tx is not valid for our chain, but may be so later on
+                    # so lets try another Tx
+                    pass
 
-    def processUnverifiedTransaction(self, transaction):
-        print(self.name + " is processing new transaction " + str(transaction))
+    def sendChain(self, chain):
+        self.q.put(chain)
 
-    def addBlockToQueue(self, block):
-        # target function of the thread class
-        self.q.put(block)
+    def recieveChain(self):
+        newChain=self.q.get()
+        if(len(newChain.blocks) > len(self.chain.blocks)):
+            if(Chain.validate(newChain, self.chain.blocks[0])):
+                self.chain=copy.deepcopy(newChain)
 
-    def processBlock(self):
-        # target function of the thread class
-        block = self.q.get()
-        print(self.name + " recieved block " + block)
-
-    def broadcastBlock(self, block):
-        print(self.name + " broadcasting block " + block)
+    def broadcastChain(self):
+        print(self.name + " broadcasting chain ")
         global nodes
         for nodeKey, node in nodes.items():
             if(nodeKey != self.name):
-                node.addBlockToQueue(block)
+                node.sendChain(self.chain)
 
 
 def idxToKey(idx):
@@ -92,10 +223,10 @@ def idxToKey(idx):
 
 def startNodes(num):
     for i in range(num):
-        key = idxToKey(i)
+        key=idxToKey(i)
         # Create node in thread
         global nodes
-        nodes[key] = Node(key)
+        nodes[key]=Node(key, )
         # Start it
         nodes[key].start()
 
@@ -107,6 +238,6 @@ if(__name__ == "__main__"):
         exit(0)
     # get data
     with open(sys.argv[1]) as json_file:
-        txs = json.loads(json_file.read())
-    NUM_NODES = 3
+        txs=json.loads(json_file.read())
+    NUM_NODES=3
     driver(txs, NUM_NODES)
